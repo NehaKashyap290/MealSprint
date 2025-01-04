431de946344490ccdba91bc61aa265d560a2103b
NehaKashyap290 <133773977+NehaKashyap290@users.noreply.github.com>
2025-01-04 15:29:33 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-require("dotenv").config();
+import { config } from 'dotenv';
+config();
 
 export const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URL).then(()=>console.log("DB Connected"));
